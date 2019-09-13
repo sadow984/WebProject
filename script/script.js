86ad78a3dcd28a29c67003073abb06b540cffa1f
Sanjeev <sanjeevsharma2552@gmail.com>
2019-09-13 15:12:47 +0530

    Pop-up form created with different style and background color.

--- script/script.js
+++ script/script.js
@@ -1,17 +1,7 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+    document.getElementById("my-form").style.display = "block";
 }
 
 function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
-
-// for close
-
-var popup = document.getElementById("abc");
-
-window.onclick = function(even) {
-    if (even.target === popup) {
-        popup.style.display = "none";
-    }
+    document.getElementById("my-form").style.display = "none";
 }
