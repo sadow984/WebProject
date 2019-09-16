48240965c595d6a66dc76385cb03af77fcefc18b
sadow984 <47876475+sadow984@users.noreply.github.com>
2019-09-17 00:06:31 +0530

    Add files via upload
    Corrected Form Validation

--- script/validate.js
+++ script/validate.js
@@ -1,22 +1,33 @@
-function validate(){
+function validate() {
     var ID = document.getElementById('ID').value;
-    var regEx_id = /^[1-7]{2}[4,9,2]{3}[0-9]{5}$/
-    var ID_result= regEx_id.test(ID);
+    var regEx_id = /^[1|2]{1}[6-9]{1}[4,9,2]{3}[0-9]{5}$/
+    var ID_result = regEx_id.test(ID);
 
     var pass = document.getElementById('password').value;
-    var regEx_password = /^(([\w\d])+([\.|\_|\@])?([\w\d])*){6,}$/
-    var password_result= regEx_password.test(pass);
-    
-    if(!ID_result){
-        document.getElementById('error_id').innerHTML =  "Please Enter Correct ID";
-        return false;
+    var regEx_password = /^([$&+,:;=?@#|'<>.^*()%!-\w\d])+$/
+    var password_result = regEx_password.test(pass);
+
+    if (!ID_result) {
+        if (ID.length != 10) {
+            document.getElementById('error_id').innerHTML = "ID length must be of 10 digits";
+            return false;
+        }
+        // else if(ID.value[1] !=1 || ID.value[0] !=2){
+        //     document.getElementById('error_id').innerHTML = "First digit must be either 1 or 2";
+        //     return false;
+
+        
+        else {
+            document.getElementById('error_id').innerHTML = "Please Enter Correct ID";
+            return false;
+         }
     }
-    else if(!password_result){
-        document.getElementById('error_password').innerHTML =  "minimum length range is 6 and must contain integers,alphabets and anyone of {'.','@','_'}";
+    else if (!password_result) {
+        // document.getElementById('error_password').innerHTML =  "";
         return false;
     }
 
-    else{
+    else {
         alert('Success');
     }
 }
